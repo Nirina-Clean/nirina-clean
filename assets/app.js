4dc6b143d99ed2e0a22aae6db281036cd5343940
Nirina-Clean <nirinaclean@gmail.com>
2025-10-08 02:27:40 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,1 @@
 
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.site-nav');
-if (toggle && nav) {
-  toggle.addEventListener('click', () => {
-    const open = nav.classList.toggle('open');
-    toggle.setAttribute('aria-expanded', open ? 'true' : 'false');
-  });
-}
